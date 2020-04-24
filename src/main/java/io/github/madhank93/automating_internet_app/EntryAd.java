7e367885bb65e3110292e797a2e08c751b7f9de3
Madhan Kumaravel <maddymadhan946@gmail.com>
2020-04-24 21:56:23 +0530

    added scripts for Entry Ad

--- src/main/java/io/github/madhank93/automating_internet_app/EntryAd.java
+++ src/main/java/io/github/madhank93/automating_internet_app/EntryAd.java
@@ -1,9 +1,14 @@
 package io.github.madhank93.automating_internet_app;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class EntryAd {
 
@@ -14,12 +19,24 @@ public class EntryAd {
 	public void initialSetup() {
 		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
 		driver = new ChromeDriver();
-		driver.get("https://the-internet.herokuapp.com/dropdown");
+		driver.get("https://the-internet.herokuapp.com/entry_ad");
 	}
 
 	@AfterTest
 	public void quit() {
 		// driver.quit();
+	}
+	
+	@Test
+	public void closeEntryAd() {
+		
+		By closeButton = By.xpath("//p[contains(text(),'Close')]");
+		
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(closeButton));
+		
+		driver.findElement(closeButton).click();
+		
 	}
 	
 }
